fa97b607e9c266b54d0634070a24c3a42ad41a01
Fatima Mohammad Afzal <60079229@udst.edu.qa>
2024-01-31 11:02:09 +0300

    function year added

--- cars.py
+++ cars.py
@@ -2,7 +2,12 @@ import random
 def printCar(cars):
     randomCar = random.choice(cars)
     print(randomCar)
+def printYear(years):
+    randomYear = random.choice(years)
+    print(randomYear)
         
 
 carList = ["Ford", "Nissan", "Toyota"]
+yearList = [2005,2023,2021]
 printCar(carList)
+printYear(yearList)
